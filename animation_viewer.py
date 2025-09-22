e726e8d48d0d6f45a196a30b441415206ff4cd57
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-09-22 12:48:46 +0900

    Making Kirby_looking Status

--- animation_viewer.py
+++ animation_viewer.py
@@ -8,9 +8,16 @@ kirby = load_image('kirby.png')
 frame = 0
 
 while True:
-    for x in range(10, 241, 33):
+    # for x in range(8, 239, 33):
+    #     clear_canvas()
+    #     kirby.clip_draw(x, 1120, 30, 30, 400, 300, 300, 300)
+    #     update_canvas()
+    #     frame = (frame + 1) % 8
+    #     delay(0.2)
+
+    for x in range(8, 239, 33):
         clear_canvas()
-        kirby.clip_draw(x, 1120, 30, 30, 400, 300, 100, 100)
+        kirby.clip_draw(x, 1054, 33, 33, 400, 300, 300, 300)
         update_canvas()
         frame = (frame + 1) % 8
         delay(0.2)
