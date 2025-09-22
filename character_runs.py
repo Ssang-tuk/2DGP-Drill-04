d630640b814f37a637fc6399b5c468f114c0efd2
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-09-22 12:23:54 +0900

    In-Class Commit

--- character_runs.py
+++ character_runs.py
@@ -5,8 +5,25 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
-# fill here
+frame = 0
 
+while True:
+
+    for x in range(0, 800, 10):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.02)
+
+    for x in range(800, 0, -10):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_composite_draw(frame * 100, 0, 100, 100, 0, 'h', x, 90, 100, 100)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.02)
 
 close_canvas()
 
