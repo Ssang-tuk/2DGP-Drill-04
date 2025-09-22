58fea2d1ee24794017a7360804f8e2b1c32b0ecb
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-09-22 12:59:14 +0900

    Change Size, Making Infinity Loop

--- animation_viewer.py
+++ animation_viewer.py
@@ -8,31 +8,31 @@ kirby = load_image('kirby.png')
 frame = 0
 
 while True:
-    # for x in range(8, 239, 33):
-    #     clear_canvas()
-    #     kirby.clip_draw(x, 1120, 30, 30, 400, 300, 300, 300)
-    #     update_canvas()
-    #     frame = (frame + 1) % 8
-    #     delay(0.2)
+    for x in range(10, 241, 33):
+        clear_canvas()
+        kirby.clip_draw(x, 1120, 30, 30, 400, 300, 400, 400)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.2)
 
-    # for x in range(8, 239, 33):
-    #     clear_canvas()
-    #     kirby.clip_draw(x, 1054, 33, 33, 400, 300, 300, 300)
-    #     update_canvas()
-    #     frame = (frame + 1) % 8
-    #     delay(0.2)
+    for x in range(8, 239, 33):
+        clear_canvas()
+        kirby.clip_draw(x, 1054, 33, 33, 400, 300, 400, 400)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.2)
 
 
-    # for x in range(8, 328, 32):
-    #     clear_canvas()
-    #     kirby.clip_draw(x, 1021, 32, 32, 400, 300, 300, 300)
-    #     update_canvas()
-    #     frame = (frame + 1) % 8
-    #     delay(0.2)
+    for x in range(8, 328, 32):
+        clear_canvas()
+        kirby.clip_draw(x, 1019, 32, 32, 400, 300, 400, 400)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.2)
 
     for x in range(7, 347, 34):
         clear_canvas()
-        kirby.clip_draw(x, 980, 34, 34, 400, 300, 300, 300)
+        kirby.clip_draw(x, 980, 34, 34, 400, 300, 400, 400)
         update_canvas()
         frame = (frame + 1) % 8
         delay(0.2)
